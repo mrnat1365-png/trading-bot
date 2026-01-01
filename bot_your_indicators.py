3023bf205536dda9e8bc2504547c6fbf45e49d5c
mrnat1365-png <mr.nat1365@gmail.com>
2026-01-01 20:40:48 +0330

    Update bot_your_indicators.py

--- bot_your_indicators.py
+++ bot_your_indicators.py
@@ -14,7 +14,7 @@ import asyncio
 from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
 TELEGRAM_TOKEN="8492497660:AAGQgmKTjrxi4c4IaRh6xg8PF9ZEYmbnZEc"
-TELEGRAM_CHAT_ID="138228682"
+CHAT_ID="138228682"
 
 # ==================== لیست ارزها ====================
 CRYPTO_PAIRS = {
@@ -775,6 +775,7 @@ def main():
 if __name__ == '__main__':
 
     main()
+
 
 
 
